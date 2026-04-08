d35cc48f80beb1f3119b96413801397fd6212c74
ZAWAWI IRFAN BIN ZAINUDDIN <zawawizainuddin@gmail.com>
2026-04-08 07:53:51 +0000

    updated the about section and added more social media links in the App.jsx file. The about section now

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,19 +24,21 @@ const imageAltText = "Fox";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a Mobile Computing student studying at Universiti Malaysia Terengganu. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a Mobile Computing student studying at Universiti Malaysia Terengganu. I have a passion for UI/UX design and front end development, and I am always looking for new ways to learn and grow in these areas. I have experience working on a variety of projects, including web design, user experience, inclusive design, focus group testing, mobile user interfaces, and graphic design. I am excited to continue learning and growing in these areas, and I am always looking for new opportunities to apply my skills and knowledge. I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "UI/UX Design",
+  "Front End Development",
+  "Web Design",
+  "User Experience",
+  "Inclusive Design",
+  "Focus Group Testing",
+  "Mobile User Interfaces",
+  "Graphic Design",
 ];
 
 /**
